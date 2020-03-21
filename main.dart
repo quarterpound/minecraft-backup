dae5da5253d82f27961e8de5957a027d4f3addf4
Ali Gasimzade <gasimzadeali@gmail.com>
2020-03-22 03:25:27 +0400

    first commit

---
+++ main.dart
@@ -0,0 +1,32 @@
+import 'dart:io';
+import 'package:intl/intl.dart';
+import 'package:archive/archive_io.dart';
+import 'package:teledart/teledart.dart';
+import 'package:teledart/telegram.dart';
+import 'package:dotenv/dotenv.dart' show load, clean, isEveryDefined, env;
+import "dart:async";
+
+void main() async {
+  load();
+  var bot = new Telegram(env['BOT']);
+
+  var stream = new Stream.periodic(const Duration(days: 1, seconds: 10), (count) async {
+    var check = await  Directory("world").exists();
+    if(check) {
+      var datestring = new DateFormat("yyyy-MM-dd").format(new DateTime.now());
+      var encoder = ZipFileEncoder();
+      encoder.zipDirectory(Directory('world'), filename: 'backup/' + datestring + '.zip');
+      return datestring;
+    }
+    return '';
+  });
+
+  stream.listen((result) async {
+    var status = await result;
+    if(status == '') {
+      bot.sendMessage(int.parse(env['CHAT_ID']), 'Back was not generated!');
+      return;
+    }
+    bot.sendMessage(int.parse(env['CHAT_ID']), 'Back up generated ${status}');
+  });
+}
